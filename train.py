9de4e63abe2fb702fdbf5bbba686c29c86825f82
sc <550786402@qq.com>
2021-11-24 10:32:55 +0800

    添加权重文件保jixu存和加载已有权重继续finetune训练功能

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from models.models import *
 from datasets.dataset import *
 from utils.loss import *
+import time
 
 if __name__ == '__main__':
     # Dataset
@@ -14,13 +15,28 @@ if __name__ == '__main__':
     cuda = torch.cuda.is_available()
     device = torch.device('cuda:0' if cuda else 'cpu')
     yolov3net = Yolov3('cfg/yolov3.cfg')
-    yolov3net.to(device).train()
-    loss = YoloLoss(yolov3net)
+    yolov3net = yolov3net.to(device)
     optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, yolov3net.parameters()), lr=1e-4, weight_decay=5e-4)
+    start_epoch = 0
+    resume = True
+    if resume:
+        # yolov3net = yolov3net.cuda()
+        checkpoint = torch.load('checkpoints/epoch40.pt')
+        yolov3net.load_state_dict(checkpoint['model'])
+        optimizer = torch.optim.Adam(yolov3net.parameters())
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        start_epoch = checkpoint['epoch'] + 1
+        print('resume done*********************')  
+    
+    
+    yolov3net.train()
+    loss = YoloLoss(yolov3net)
 
-    for epoch in range(1000):
+    for epoch in range(start_epoch,100):    
+        print('epoch {}'.format(epoch))
+        t0 = time.time()
         for data in dataloader:
-            imgs,labels = data
+            imgs,labels,_ = data
             imgs = imgs.to(device)
             imgs = imgs.float()/255.
             # print(imgs.shape,labels.shape)
@@ -34,4 +50,15 @@ if __name__ == '__main__':
             optimizer.zero_grad() #清空梯度
             total_loss.backward() #反向传播
             optimizer.step()      #更新参数
-            
+        
+        t1 = time.time()
+        print('epoch{} train for {}'.format(epoch,(t1-t0)))
+
+        checkpoint = {'epoch':epoch,
+                      'model':yolov3net.state_dict(),
+                      'optimizer':optimizer.state_dict()}
+        if epoch % 5 == 0:
+            checkpoint_name = 'checkpoints/epoch{}.pt'.format(epoch)
+            torch.save(checkpoint,checkpoint_name)
+
+        
